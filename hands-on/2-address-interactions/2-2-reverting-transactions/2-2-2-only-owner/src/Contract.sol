b13974bf548e049738a844da8570f14297c58723
Jason Yapri <jason.yapri@gmail.com>
2024-02-19 21:48:21 +0700

    2-2-2-only-owner

--- hands-on/2-address-interactions/2-2-reverting-transactions/2-2-2-only-owner/src/Contract.sol
+++ hands-on/2-address-interactions/2-2-reverting-transactions/2-2-2-only-owner/src/Contract.sol
@@ -2,11 +2,21 @@
 pragma solidity ^0.8.20;
 
 contract Contract {
+    address deployer;
     error InsufficientEther(uint256);
+    error NotAuthorized();
 
     constructor() payable {
-        if (msg.value != 1 ether) {
-            revert InsufficientEther(msg.value);
+        deployer = msg.sender;
+        if (msg.value < 1 ether) revert InsufficientEther(msg.value);
+    }
+
+    function withdraw() public {
+        if (deployer == msg.sender) {
+            (bool success, ) = deployer.call{value: address(this).balance}("");
+            require(success);
+        } else {
+            revert NotAuthorized();
         }
     }
 }
