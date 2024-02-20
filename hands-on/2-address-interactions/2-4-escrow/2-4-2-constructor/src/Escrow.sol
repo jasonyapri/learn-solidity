b76a2b33ac60490588a9dba740c62a24762d757f
Jason Yapri <jason.yapri@gmail.com>
2024-02-20 18:23:13 +0700

    2-4-2-constructor

---
+++ hands-on/2-address-interactions/2-4-escrow/2-4-2-constructor/src/Escrow.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Escrow {
+    address public depositor = msg.sender;
+    address public beneficiary;
+    address public arbiter;
+
+    constructor(address _arbiter, address _beneficiary) {
+        arbiter = _arbiter;
+        beneficiary = _beneficiary;
+    }
+}
