acc55e6b5e6d0dab1a90e14a8356aa7cd7f41d09
Jason Yapri <jason.yapri@gmail.com>
2024-02-22 16:48:19 +0700

    3-1-5-stack-club-1

---
+++ hands-on/3-reference-types/3-1-arrays/3-1-5-stack-club-1/src/Contract.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract StackClub {
+    address[] public members;
+
+    function addMember(address newMember) external {
+        members.push(newMember);
+    }
+
+    function isMember(address currentMember) public view returns(bool) {
+        for (uint i = 0; i < members.length; i++) {
+            if (members[i] == currentMember) return true;
+        }
+
+        return false;
+    }
+}
