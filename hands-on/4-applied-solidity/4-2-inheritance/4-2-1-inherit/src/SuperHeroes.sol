b17df9e1ca62f3b57370973acab4b98b4ae1deea
Jason Yapri <jason.yapri@gmail.com>
2024-02-24 16:18:14 +0700

    4-2-1-inherit

---
+++ hands-on/4-applied-solidity/4-2-inheritance/4-2-1-inherit/src/SuperHeroes.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "./Hero.sol";
+
+// TODO: create Mage/Warrior Heroes
+contract Mage is Hero {
+    
+}
+
+contract Warrior is Hero {
+    
+}
