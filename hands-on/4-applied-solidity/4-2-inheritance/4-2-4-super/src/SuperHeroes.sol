5a7f62df4cb169b9189e1416a31815506b5070c5
Jason Yapri <jason.yapri@gmail.com>
2024-02-24 16:48:50 +0700

    4-2-4-super

---
+++ hands-on/4-applied-solidity/4-2-inheritance/4-2-4-super/src/SuperHeroes.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "./Hero.sol";
+
+// TODO: create Mage/Warrior Heroes
+contract Mage is Hero(50) {
+    function attack(Enemy enemy) override public {
+        super.attack(enemy);
+        enemy.takeAttack(AttackTypes.Spell);
+    }
+}
+
+contract Warrior is Hero(200) {
+    function attack(Enemy enemy) override public {
+        super.attack(enemy);
+        enemy.takeAttack(AttackTypes.Brawl);
+    }
+}
