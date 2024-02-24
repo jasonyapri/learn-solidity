a5ec27555d0476ddcc24f53e83e4a6c87d1f5452
Jason Yapri <jason.yapri@gmail.com>
2024-02-24 14:07:24 +0700

    4-1-3-multiple-vote

---
+++ hands-on/4-applied-solidity/4-1-voting-contract/4-1-3-multiple-vote/src/Voting.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Voting {
+
+    struct Proposal {
+        address target;
+        bytes data;
+        uint yesCount;
+        uint noCount;
+        mapping(address => uint8) voters;
+    }
+    
+    Proposal[] public proposals;
+
+    function newProposal(address _target, bytes calldata _data) external {
+        Proposal storage newProp = proposals.push();
+
+        newProp.target = _target;
+        newProp.data = _data;
+        newProp.yesCount = 0;
+        newProp.noCount = 0;
+    }
+
+    function castVote(uint proposalId, bool support) external {
+        if (proposals[proposalId].voters[msg.sender] == 1) {
+            if (!support) {
+                proposals[proposalId].yesCount--;
+                proposals[proposalId].noCount++;
+                proposals[proposalId].voters[msg.sender] = 2;
+            }
+        } else if (proposals[proposalId].voters[msg.sender] == 2) {
+            if (support) {
+                proposals[proposalId].yesCount++;
+                proposals[proposalId].noCount--;
+                proposals[proposalId].voters[msg.sender] = 1;
+            }
+        } else{
+            if (support) {
+                proposals[proposalId].yesCount++;
+                proposals[proposalId].voters[msg.sender] = 1;
+            } else{
+                proposals[proposalId].noCount++;
+                proposals[proposalId].voters[msg.sender] = 2;
+            }
+        }
+    }
+}
